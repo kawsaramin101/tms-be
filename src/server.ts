eb58cf4fa109ad5b979141e781add16b601d1053
kawsaramin101 <71335212+kawsaramin101@users.noreply.github.com>
2026-09-19 11:27:25 +0600

    Initialized

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.json({
+    message: "Hello from Express + TypeScript!"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
